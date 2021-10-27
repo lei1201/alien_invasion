984ffe102120d4538bfa94e2f654549571749f7b
jiangll <1158382938@qq.com>
2021-10-27 15:48:55 +0800

    创建了ship文件，创建了Ship类，用于设定飞船位置

---
+++ ship.py
@@ -0,0 +1,27 @@
+#！/usr/bin/env python
+# -*- coding:utf-8 -*-
+# _time_='2021/10/27 15:36'
+# _PROJECT_NAME_=' alien_invasion '
+# _NAME_=' ship '
+# _USER_=' ThinkPad E15 '
+
+import pygame
+
+class Ship():
+
+    def __init__(self):
+        """初始化飞船并设定其初始位置"""
+        self.screen = screen
+
+        #加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        #将每艘飞船放在屏幕底部中央
+        self.rect.centerx = self.screen_rect.centerx
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image,self.rect)
