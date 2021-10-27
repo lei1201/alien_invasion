3eef1992f57a10ab55504a9ff295073a44a9f730
jiangll <1158382938@qq.com>
2021-10-27 16:45:43 +0800

    修改主程序中监视鼠标键盘操作的函数，直接用check_events代替

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
     """用于初始化游戏"""
@@ -26,9 +27,7 @@ def run_game():
     while True:
 
         #监视键盘和鼠标事件
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+        gf.check_events()
 
         #每次循环都会重绘屏幕
         screen.fill(ai_settings.bg_color)
