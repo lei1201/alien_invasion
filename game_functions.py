a26bc70681d9909c959dd6675a306b383c9710e0
jiangll <1158382938@qq.com>
2021-10-28 15:25:36 +0800

    更新了game_functions文件，重构了check_events函数，使其代码结构更清晰

--- game_functions.py
+++ game_functions.py
@@ -8,6 +8,20 @@
 import sys
 import pygame
 
+def check_keydown_events(event,ship):
+    """"响应按键"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event,ship):
+    """"响应松开"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
 def check_events(ship):
     """响应键盘和鼠标事件"""
     for event in pygame.event.get():
@@ -15,16 +29,10 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event,ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
 
 def update_screen(ai_settings,screen,ship):
     """更新屏幕上的图像，并切换到新屏幕"""
