5b74c02112082f63a1db8cb4b5f87e10600f4893
jiangll <1158382938@qq.com>
2021-10-29 16:08:17 +0800

    更新主程序文件，放多行外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -29,7 +29,7 @@ def run_game():
     aliens = Group()
 
     # 创建一个外星人编组
-    gf.create_fleet(ai_settings,screen,aliens)
+    gf.create_fleet(ai_settings,screen,ship,aliens)
 
     #开始游戏的主循环
     while True:
