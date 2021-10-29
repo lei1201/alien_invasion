32d43142e1b31574e31512c12250f35fd3c16ea5
jiangll <1158382938@qq.com>
2021-10-29 15:35:36 +0800

    更新settings文件，更正其中的参数设置

--- settings.py
+++ settings.py
@@ -11,8 +11,8 @@ class Settings():
     def __init__(self):
         """初始化游戏的设置"""
         #屏幕设置
-        self.scree_width = 1200
-        self.scree_hight = 800
+        self.screen_width = 1200
+        self.screen_height = 800
         self.bg_color = (230,230,230)
 
         #飞船的设置
