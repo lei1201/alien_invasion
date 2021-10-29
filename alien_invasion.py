75d88ba5e264c635f928ec6a24df5d3cb1654d73
jiangll <1158382938@qq.com>
2021-10-29 15:03:08 +0800

    更新主程序，导入了外星人

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -26,6 +27,9 @@ def run_game():
 
     #创建一个用于存储子弹的编组
     bullets = Group()
+
+    # 创建一个外星人
+    alien = Alien(ai_settings,screen)
 
     #开始游戏的主循环
     while True:
