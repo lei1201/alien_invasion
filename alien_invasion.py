502a257d2c0cae5c76afd67ce6875adc676fbb41
jiangll <1158382938@qq.com>
2021-10-29 17:48:21 +0800

    更新alien、game_functions、settings、game_status文件，飞船用完之后，将在界面卡住不动

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,7 +11,7 @@ from settings import Settings
 from ship import Ship
 import game_functions as gf
 from pygame.sprite import Group
-from game_stats import GameStatus
+from game_stats import GameStats
 
 def run_game():
     """用于初始化游戏"""
@@ -23,7 +23,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一个用于存储游戏统计信息的实例
-    status = GameStatus(ai_settings)
+    stats = GameStats(ai_settings)
 
     # 创建一艘飞船
     ship = Ship(ai_settings,screen)
@@ -39,9 +39,12 @@ def run_game():
     while True:
 
         gf.check_events(ai_settings,screen,ship,bullets)
-        ship.update()
-        gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
-        gf.update_aliens(ai_settings,status,screen,ship,aliens,bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
         gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
--- game_stats.py
+++ game_stats.py
@@ -5,14 +5,16 @@
 # _NAME_=' game_stats '
 # _USER_=' ThinkPad E15 '
 
-class GameStatus():
+class GameStats():
     """跟踪游戏的统计信息"""
 
     def __init__(self,ai_settings):
         """初始化统计信息"""
         self.ai_settings = ai_settings
-        self.reset_status()
+        self.reset_stats()
+        #游戏刚启动时处于活跃状态
+        self.game_active = True
 
-    def reset_status(self):
+    def reset_stats(self):
         """初始化在游戏运行期间可能变化的统计信息"""
         self.ship_left = self.ai_settings.ship_limit
