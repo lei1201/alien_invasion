c1bfa44475fa6174024149423445a5b635b9eee5
jiangll <1158382938@qq.com>
2021-10-29 15:36:56 +0800

    更新主程序文件，新建机器人群

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,7 +9,6 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
-from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -19,7 +18,7 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-        (ai_settings.scree_width,ai_settings.scree_hight))
+        (ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     #创建一艘飞船
@@ -27,9 +26,10 @@ def run_game():
 
     #创建一个用于存储子弹的编组
     bullets = Group()
+    aliens = Group()
 
-    # 创建一个外星人
-    alien = Alien(ai_settings,screen)
+    # 创建一个外星人编组
+    gf.create_fleet(ai_settings,screen,aliens)
 
     #开始游戏的主循环
     while True:
@@ -37,6 +37,6 @@ def run_game():
         gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings,screen,ship,alien,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
